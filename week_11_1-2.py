a0b4917c72390622f82b9826a634c4cccc414e3b
Batuhan ÖZALP <33182788+bozalp@users.noreply.github.com>
2019-05-10 16:29:58 +0300

    Update week_11_1-2.py

--- week_11_1-2.py
+++ week_11_1-2.py
@@ -108,6 +108,30 @@ def bubbleSort(arr):
 
     return arr,yer_degistirme_sayisi,karsilastirma_sayisi
 
+def shell_sort(arr):
+  n = len(arr)
+  gap = int(n / 2)
+  karsilastirma_sayisi = 0
+  swap_sayisi = 0
+  while(gap > 0):
+    for i in range(gap,n):
+      karsilastirma_sayisi += 1
+      temp = arr[i]
+      j = i
+      while j >= gap and arr[j - gap] > temp:
+          swap_sayisi += 1
+          karsilastirma_sayisi += 1
+          arr[i] = arr[j - gap]
+          j -= gap
+      arr[j] = temp
+    gap = gap // 2
+  return arr,karsilastirma_sayisi,swap_sayisi
+
+a = get_n_random_integer(100)
+print(a)
+print(shell_sort(a))
+
+
 sayilar = get_n_random_integer(10)
 print("sayilar \t: ",sayilar)
 print("ortalama \t: ",get_mean_for_n_integer(sayilar))
